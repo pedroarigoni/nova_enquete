0e8b74f0af95e3f16b9f22e934e6545d111bbd5d
pedroargioni <pedroargioni@gmail.com>
2024-01-23 18:05:17 -0300

    configured models

--- enquetes/models.py
+++ enquetes/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Questao(models.Model):
+    titulo_questao = models.CharField(max_length=200)
+    data_pub = models.DateTimeField('date published')
+
+
+class Opcao(models.Model):
+    questao = models.ForeignKey(Questao, on_delete=models.CASCADE)
+    titulo_opcao = models.CharField(max_length=200)
+    votos = models.IntegerField(default=0)
