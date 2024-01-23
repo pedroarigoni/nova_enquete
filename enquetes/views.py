83ee482dc350a7b68f1fbf2744118c7ebeb69d6d
pedroargioni <pedroargioni@gmail.com>
2024-01-23 18:39:04 -0300

    created new views

--- enquetes/views.py
+++ enquetes/views.py
@@ -4,3 +4,16 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse('Olá, Index!')
+
+
+def detalhes(request, questao_id):
+    return HttpResponse('Questão : %s.' % questao_id)
+
+
+def resultados(request, questao_id):
+    response = 'Questão: %s.'
+    return HttpResponse(response % questao_id)
+
+
+def votos(request, questao_id):
+    return HttpResponse('Você votou na questão: %s.' % questao_id)
